7309afc325a06b23cde1f975d9db21daa08968cc
Faisal Ahmed <faisal@squarehealth.com.bd>
2024-09-08 10:42:35 +0600

    4th commit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const {generatePdf} = require("../controllers/generatePdf");
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send('Welcome');
+});
+
 router.post('/generatePdf', generatePdf);
 
 module.exports = router;
